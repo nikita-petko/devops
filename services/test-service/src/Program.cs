53e8ea379e4fbe303c3ca125e9b9d114155483ec
Nikita Petko <petko@vmminfra.net>
2024-07-01 09:28:06 +0100

    Update Program.cs
    #!components: test-service

--- services/test-service/src/Program.cs
+++ services/test-service/src/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Run in an infinite loop
 while (true)
 {
-    Console.WriteLine(Environment.GetEnvironmentVariable("MESSAGE") ?? "Hello, World!");
+    Console.WriteLine(Environment.GetEnvironmentVariable("MESSAGE") ?? "Hello, World 1!");
 
     Thread.Sleep(1000);
 }
